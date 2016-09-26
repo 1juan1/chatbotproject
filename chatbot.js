b667279901e672bd3dfbe629564cb3459839d75d
Juan Yepez <yepezjuan6@gmail.com>
2016-09-25 20:23:28 -0700

    making sure it is safe

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,30 @@
+var date = new Date();
+var weekday = date.getUTCDay();
+var month = date.getMonth();
+var year = date.getFullYear();
+var day = date.getDate();
+
+var weekDayConversion = {
+0: "Sunday",
+1: "Monday",
+2:"Tuesday",
+3:"Wednsday",
+4:
+
+}
+
+
 function askQuestion(){
   var input = document.getElementById("input").value;
-  document.getElementById("chat-area").textContent += input;
+  var giveresponse = responses[input]
+  document.getElementById("chat-area").innerHTML += input + ": " + giveresponse + "<br>";
+
+}
+
+
+
+ var responses = {
+  "Hey how are you?"  : "         did you just assume my gender"
+
+
 }
