dae068231d3d6b31e60bbced0933310e434aac7a
Juan Yepez <yepezjuan6@gmail.com>
2016-09-27 11:29:45 -0700

    added the month and  yea and day

--- chatbot.js
+++ chatbot.js
@@ -4,13 +4,31 @@ var month = date.getMonth();
 var year = date.getFullYear();
 var day = date.getDate();
 
-var weekDayConversion = {
+var weekDayConversion  = {
 0: "Sunday",
 1: "Monday",
 2:"Tuesday",
 3:"Wednsday",
-4:
+4:"Thursday",
+5:"Friday",
+6:"Saturday",
 
+
+}
+
+var monthConversion = {
+  0: "January",
+  1: "Febuary",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December"
 }
 
 
@@ -24,7 +42,13 @@ function askQuestion(){
 
 
  var responses = {
-  "Hey how are you?"  : "         did you just assume my gender"
+  "Hey how are you?"  : "         did you just assume my gender",
+  "how old are you" : "ugh i have a   boyfriend",
+  "what is your age " : "i havvvvve no age ",
 
+"do u like chocolate": "yea i like dark chocolate",
+"what is the date": "The day today is " + weekDayConversion[weekday],
+"what month are we in" : "we are in " + monthConversion[month],
+"what year is it": "i think it is " + [year]
 
 }
